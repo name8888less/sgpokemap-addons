e043d5466202eff44b38d7361f693251b2b7134f
name8888less <127593561+name8888less@users.noreply.github.com>
2024-01-19 12:41:02 +0800

    Create annihilape-pvp-filter.js
    Added Annihilate Filter

---
+++ annihilape-pvp-filter.js
@@ -0,0 +1,410 @@
+var mankeyGL = [
+    {iv: "2/15/15", lvl: 17},
+    {iv: "2/15/14", lvl: 17},
+    {iv: "3/14/14", lvl: 17},
+    {iv: "0/15/5", lvl: 17.5},
+    {iv: "3/15/13", lvl: 17},
+    {iv: "0/12/9", lvl: 17.5},
+    {iv: "0/8/14", lvl: 17.5},
+    {iv: "1/15/15", lvl: 17},
+    {iv: "1/15/14", lvl: 17},
+    {iv: "0/9/13", lvl: 17.5},
+    {iv: "0/13/7", lvl: 17.5},
+    {iv: "2/14/15", lvl: 17},
+    {iv: "2/14/14", lvl: 17},
+    {iv: "3/13/15", lvl: 17},
+    {iv: "3/13/14", lvl: 17},
+    {iv: "0/10/11", lvl: 17.5},
+    {iv: "4/12/14", lvl: 17},
+    {iv: "0/14/6", lvl: 17.5},
+    {iv: "0/14/5", lvl: 17.5},
+    {iv: "2/15/13", lvl: 17},
+    {iv: "1/13/5", lvl: 17.5},
+    {iv: "3/14/13", lvl: 17},
+    {iv: "0/11/10", lvl: 17.5},
+    {iv: "0/11/9", lvl: 17.5},
+    {iv: "0/7/15", lvl: 17.5},
+    {iv: "0/7/14", lvl: 17.5},
+    {iv: "4/13/13", lvl: 17},
+    {iv: "0/15/4", lvl: 17.5},
+    {iv: "1/10/9", lvl: 17.5},
+    {iv: "0/15/15", lvl: 17},
+    {iv: "0/15/14", lvl: 17},
+    {iv: "1/6/15", lvl: 17.5},
+    {iv: "1/6/14", lvl: 17.5},
+    {iv: "1/14/4", lvl: 17.5},
+    {iv: "1/14/15", lvl: 17},
+    {iv: "1/14/14", lvl: 17},
+    {iv: "3/15/12", lvl: 17},
+    {iv: "3/15/11", lvl: 17},
+    {iv: "0/8/13", lvl: 17.5},
+    {iv: "0/12/8", lvl: 17.5},
+    {iv: "0/12/7", lvl: 17.5},
+    {iv: "2/5/14", lvl: 17.5},
+    {iv: "2/13/15", lvl: 17},
+    {iv: "2/13/14", lvl: 17},
+    {iv: "4/14/12", lvl: 17},
+    {iv: "4/14/11", lvl: 17},
+    {iv: "1/11/8", lvl: 17.5},
+    {iv: "1/11/7", lvl: 17.5},
+    {iv: "1/7/13", lvl: 17.5},
+    {iv: "3/12/15", lvl: 17},
+    {iv: "3/12/14", lvl: 17},
+    {iv: "5/13/11", lvl: 17},
+    {iv: "2/10/7", lvl: 17.5},
+    {iv: "1/15/13", lvl: 17},
+    {iv: "1/15/3", lvl: 17.5},
+    {iv: "1/15/2", lvl: 17.5},
+    {iv: "0/9/12", lvl: 17.5},
+    {iv: "0/9/11", lvl: 17.5},
+    {iv: "4/11/15", lvl: 17},
+    {iv: "4/11/14", lvl: 17},
+    {iv: "0/13/6", lvl: 17.5},
+    {iv: "0/13/5", lvl: 17.5},
+    {iv: "2/14/13", lvl: 17},
+    {iv: "2/14/2", lvl: 17.5},
+    {iv: "1/8/12", lvl: 17.5},
+    {iv: "1/8/11", lvl: 17.5},
+    {iv: "4/15/10", lvl: 17},
+    {iv: "4/15/9", lvl: 17},
+    {iv: "1/12/6", lvl: 17.5},
+    {iv: "1/12/5", lvl: 17.5},
+    {iv: "5/10/15", lvl: 17},
+    {iv: "5/10/14", lvl: 17},
+    {iv: "3/13/13", lvl: 17},
+    {iv: "5/14/9", lvl: 17},
+    {iv: "2/7/11", lvl: 17.5},
+    {iv: "0/10/10", lvl: 17.5},
+    {iv: "0/10/9", lvl: 17.5},
+    {iv: "2/11/6", lvl: 17.5},
+    {iv: "2/11/5", lvl: 17.5},
+    {iv: "6/9/14", lvl: 17},
+    {iv: "0/6/15", lvl: 17.5},
+    {iv: "0/6/14", lvl: 17.5},
+    {iv: "4/12/13", lvl: 17},
+    {iv: "0/14/4", lvl: 17.5},
+    {iv: "1/9/10", lvl: 17.5},
+    {iv: "1/9/9", lvl: 17.5},
+    {iv: "0/14/15", lvl: 17},
+    {iv: "0/14/14", lvl: 17},
+    {iv: "3/10/5", lvl: 17.5},
+    {iv: "2/15/12", lvl: 17},
+    {iv: "2/15/11", lvl: 17},
+    {iv: "2/15/0", lvl: 17.5},
+    {iv: "5/11/13", lvl: 17},
+    {iv: "1/5/15", lvl: 17.5},
+    {iv: "1/5/14", lvl: 17.5},
+    {iv: "1/13/4", lvl: 17.5},
+    {iv: "1/13/15", lvl: 17},
+    {iv: "1/13/14", lvl: 17},
+    {iv: "3/14/12", lvl: 17},
+    {iv: "3/14/11", lvl: 17}
+];
+
+var mankeyIvsGL = mankeyGL.map(function(item){
+    return item.iv
+});
+
+var mankeyLvlsGL = mankeyGL.map(function(item){
+    return item.lvl
+});
+
+/*UL Mankey */
+var mankeyUL = [
+    {iv: "0/15/15", lvl: 28.5},
+    {iv: "1/14/15", lvl: 28.5},
+    {iv: "1/15/14", lvl: 28.5},
+    {iv: "2/15/12", lvl: 28.5},
+    {iv: "0/10/14", lvl: 29},
+    {iv: "0/11/13", lvl: 29},
+    {iv: "0/12/11", lvl: 29},
+    {iv: "0/13/10", lvl: 29},
+    {iv: "0/14/9", lvl: 29},
+    {iv: "0/15/8", lvl: 29},
+    {iv: "0/15/7", lvl: 29},
+    {iv: "0/14/15", lvl: 28.5},
+    {iv: "1/14/7", lvl: 29},
+    {iv: "0/15/14", lvl: 28.5},
+    {iv: "1/13/15", lvl: 28.5},
+    {iv: "1/14/14", lvl: 28.5},
+    {iv: "1/15/13", lvl: 28.5},
+    {iv: "1/15/12", lvl: 28.5},
+    {iv: "2/12/15", lvl: 28.5},
+    {iv: "2/13/14", lvl: 28.5},
+    {iv: "2/14/13", lvl: 28.5},
+    {iv: "2/14/12", lvl: 28.5},
+    {iv: "3/11/15", lvl: 28.5},
+    {iv: "2/15/11", lvl: 28.5},
+    {iv: "3/13/12", lvl: 28.5},
+    {iv: "3/14/11", lvl: 28.5},
+    {iv: "3/15/9", lvl: 28.5},
+    {iv: "0/9/15", lvl: 29},
+    {iv: "0/9/14", lvl: 29},
+    {iv: "0/10/13", lvl: 29},
+    {iv: "0/11/12", lvl: 29},
+    {iv: "0/11/11", lvl: 29},
+    {iv: "0/13/2", lvl: 29.5},
+    {iv: "0/12/10", lvl: 29},
+    {iv: "0/13/9", lvl: 29},
+    {iv: "1/8/15", lvl: 29},
+    {iv: "1/8/14", lvl: 29},
+    {iv: "1/9/13", lvl: 29},
+    {iv: "1/10/12", lvl: 29},
+    {iv: "1/10/11", lvl: 29},
+    {iv: "0/14/8", lvl: 29},
+    {iv: "0/14/7", lvl: 29},
+    {iv: "1/11/10", lvl: 29},
+    {iv: "1/12/9", lvl: 29},
+    {iv: "0/15/6", lvl: 29},
+    {iv: "0/13/15", lvl: 28.5},
+    {iv: "2/7/14", lvl: 29},
+    {iv: "1/13/8", lvl: 29},
+    {iv: "1/13/7", lvl: 29},
+    {iv: "2/9/11", lvl: 29},
+    {iv: "4/15/15", lvl: 28},
+    {iv: "0/14/14", lvl: 28.5},
+    {iv: "2/10/10", lvl: 29},
+    {iv: "1/14/6", lvl: 29},
+    {iv: "0/15/13", lvl: 28.5},
+    {iv: "0/15/12", lvl: 28.5},
+    {iv: "1/12/15", lvl: 28.5},
+    {iv: "2/12/7", lvl: 29},
+    {iv: "1/13/14", lvl: 28.5},
+    {iv: "1/15/5", lvl: 29},
+    {iv: "5/14/15", lvl: 28},
+    {iv: "1/14/13", lvl: 28.5},
+    {iv: "1/14/12", lvl: 28.5},
+    {iv: "2/13/6", lvl: 29},
+    {iv: "5/15/14", lvl: 28},
+    {iv: "5/15/13", lvl: 28},
+    {iv: "2/11/15", lvl: 28.5},
+    {iv: "1/15/11", lvl: 28.5},
+    {iv: "2/12/14", lvl: 28.5},
+    {iv: "2/13/13", lvl: 28.5},
+    {iv: "2/13/12", lvl: 28.5},
+    {iv: "6/14/13", lvl: 28},
+    {iv: "2/15/3", lvl: 29},
+    {iv: "2/14/11", lvl: 28.5},
+    {iv: "3/10/15", lvl: 28.5},
+    {iv: "3/11/14", lvl: 28.5},
+    {iv: "6/15/12", lvl: 28},
+    {iv: "2/15/10", lvl: 28.5},
+    {iv: "2/15/9", lvl: 28.5},
+    {iv: "3/12/13", lvl: 28.5},
+    {iv: "3/12/12", lvl: 28.5},
+    {iv: "0/5/12", lvl: 29.5},
+    {iv: "0/6/11", lvl: 29.5},
+    {iv: "0/4/14", lvl: 29.5},
+    {iv: "0/4/13", lvl: 29.5},
+    {iv: "0/7/10", lvl: 29.5},
+    {iv: "0/7/9", lvl: 29.5},
+    {iv: "3/13/11", lvl: 28.5},
+    {iv: "0/3/15", lvl: 29.5},
+    {iv: "0/8/8", lvl: 29.5},
+    {iv: "4/9/15", lvl: 28.5},
+    {iv: "0/9/7", lvl: 29.5},
+    {iv: "4/10/14", lvl: 28.5},
+    {iv: "3/14/10", lvl: 28.5},
+    {iv: "3/14/9", lvl: 28.5},
+    {iv: "0/10/6", lvl: 29.5},
+    {iv: "0/10/5", lvl: 29.5},
+    {iv: "4/11/12", lvl: 28.5},
+    {iv: "0/8/15", lvl: 29},
+    {iv: "0/8/14", lvl: 29}
+];
+
+var mankeyIvsUL = mankeyUL.map(function(item){
+    return item.iv
+});
+
+var mankeyLvlsUL = mankeyUL.map(function(item){
+    return item.lvl
+});
+
+function refreshPokemons() {
+    if (!shouldUpdate) {
+        return; //don't update when map is moving
+    }
+    var toBeRemovedIndexes = [];
+    var currentUnixTime = Math.floor(Date.now() / 1000) - timeOffset;
+    for (var i = 0; i < pokemons.length; ++i) {
+        var currentPokemon = pokemons[i];
+        if (currentPokemon.despawn < currentUnixTime - 10 ||
+            (!isPokemonChecked(currentPokemon.id) && !shouldTurnFilterOff())) {
+            toBeRemovedIndexes.push(i);
+        }
+    }
+
+    for (var i = toBeRemovedIndexes.length - 1; i >= 0; --i) {
+        pokemons.splice(toBeRemovedIndexes[i], 1);
+        var marker = markers[toBeRemovedIndexes[i]];
+        marker.removeFrom(map);
+        markers.splice(toBeRemovedIndexes[i], 1);
+    }
+
+    //remove low IV from map, add high IV to map
+
+    for (var i = 0; i < pokemons.length; ++i) {
+        var currentPokemon = pokemons[i];
+
+        // fix to adjust for -1 attack, probably screen capture error
+        var hasValidStats = false;
+        if (currentPokemon.attack>= 1 || currentPokemon.defence >= 1 || currentPokemon.stamina >= 1 ) {
+            hasValidStats = true;
+            if (currentPokemon.attack== -1) {
+                currentPokemon.attack = 0;
+            }
+            if (currentPokemon.defence== -1) {
+                currentPokemon.defence = 0;
+            }
+            if (currentPokemon.stamina== -1) {
+                currentPokemon.stamina = 0;
+            }
+        }
+
+        var ivPercentage = (currentPokemon.attack + currentPokemon.defence + currentPokemon.stamina) / 45 * 100;
+        var marker = markers[i];
+
+        var min_iv_compare = min_iv;
+
+        //to let unknown iv show
+        if (min_iv == 0) {
+            min_iv_compare = -100;
+        }
+
+        if ((ivPercentage >= min_iv_compare && hasValidStats) || shouldTurnFilterOff()) {
+            var isDesiredPokemon = false;
+            var statsString = currentPokemon.attack+ "/"+ currentPokemon.defence +"/"+currentPokemon.stamina;
+
+            if (currentPokemon.id==56) {
+
+                var index = mankeyIvsGL.indexOf(statsString);
+                var pvpString = "";
+                if (index != -1) {
+                    //found. check level
+                    targetPokemonLevel = mankeyLvlsGL[index];
+                    if (currentPokemon.level < targetPokemonLevel)
+                    {
+                        isDesiredPokemon = true;
+                        pvpString = "GL #" + (index+1);
+                    }
+                }
+
+                index = mankeyLvlsUL.indexOf(statsString);
+                if (index!= -1) {
+                    targetPokemonLevel = mankeyLvlsUL[index];
+                    if (currentPokemon.level < targetPokemonLevel)
+                    {
+                        //found
+                        if (isDesiredPokemon) {
+                            pvpString += ", "
+                        }
+                        isDesiredPokemon = true;
+                        pvpString += "UL #" + (index+1);
+                    }
+                }
+
+                currentPokemon.pvptype = pvpString;
+
+            }
+
+            if (isDesiredPokemon) {
+                if (!marker._map) {
+                    marker.addTo(map);
+                }
+            }
+            else {
+                if (marker._map) {
+                    marker.removeFrom(map);
+                }
+            }
+        }
+        else {
+            if (marker._map) {
+                marker.removeFrom(map);
+            }
+        }
+    }
+
+    if (shouldShowTimers()) {
+        for (var i = 0; i < markers.length; ++i) {
+            //only update for the ones in bounds
+            var mapBounds = map.getBounds();
+            var tmpMarker = markers[i];
+            if (mapBounds.contains(tmpMarker.getLatLng())) {
+                $(tmpMarker._icon).find('.pokemon_icon_timer').html(timeToString(pokemons[i].remainingTime()));
+            }
+        }
+    }
+}
+
+function infoWindowString(pokemon) {
+
+    var disguiseString = "";
+    if (pokemon.disguise != 0 && pokemon.disguise != -1) {
+        disguiseString = " (" + getDisguisePokemonName(pokemon) + ")";
+    }
+
+    var genderString = getGenderString(pokemon);
+
+    var formString = getFormString(pokemon);
+
+    var ivString = "";
+
+    var movesetString = "";
+
+    var cpString = "";
+    var weatherString = "";
+
+    weatherString = "<a href='faq.html#weather'><b>Weather boost</b>: ";
+    switch (pokemon.weather) {
+        case 0:
+            weatherString += "None<br /></a>";
+            break;
+        case 1:
+            weatherString += "Clear<br /></a>";
+            break;
+        case 2:
+            weatherString += "Rainy<br /></a>";
+            break;
+        case 3:
+            weatherString += "Partly Cloudy<br /></a>";
+            break;
+        case 4:
+            weatherString += "Cloudy<br /></a>";
+            break;
+        case 5:
+            weatherString += "Windy<br /></a>";
+            break;
+        case 6:
+            weatherString += "Snow<br /></a>";
+            break;
+        case 7:
+            weatherString += "Fog<br /></a>";
+            break;
+        default:
+            weatherString = "";
+            break;
+    }
+
+    // fix to adjust for -1 attack, probably screen capture error
+    if (pokemon.attack>= 1 || pokemon.defence >= 1 || pokemon.stamina >= 1 ) {
+        if (pokemon.attack== -1) {
+            pokemon.attack = 0;
+        }
+        if (pokemon.defence== -1) {
+            pokemon.defence = 0;
+        }
+        if (pokemon.stamina== -1) {
+            pokemon.stamina = 0;
+        }
+    }
+
+    if (pokemon.attack != -1 && pokemon.defence != -1 && pokemon.stamina != -1 && pokemon.move1 != -1 && pokemon.move2 != -1 && pokemon.cp != -1) {
+        ivString = "<b>L30+ IV:</b> "+ pokemon.attack + " | " + pokemon.defence + " | " + pokemon.stamina + " (" + Math.floor((pokemon.attack + pokemon.defence + pokemon.stamina)/45 * 100) + "%)<br />";
+        movesetString = "<b>L30+ Moveset:</b><br />" + getMoveName(pokemon.move1) + " | " + getMoveName(pokemon.move2) + "<br />";
+        cpString = "<b>L30+ CP:</b> " + pokemon.cp + " (Level: " + pokemon.level + ")<br /><br />";
+    }
+
+    return '<b>' + getPokemonName(pokemon) + disguiseString + genderString + formString + "</b><br />" + (pokemon.pvptype ?   pokemon.pvptype + "</b><br /><br />" : "") + weatherString + ivString + movesetString + cpString + timeToString(pokemon.remainingTime()) + '<br /><br /><a target="_blank" href="https://maps.google.com/maps?q=' + pokemon.center.lat + ',' + pokemon.center.lng + '">Maps</a><br /><br /><a href="faq.html#approximately_timer" target="_blank">(Approximately Despawn Time)</a>';
+}
